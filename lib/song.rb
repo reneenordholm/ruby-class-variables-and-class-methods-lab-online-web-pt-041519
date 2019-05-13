ba7fad4d865af860214fd55e4f14a58f3df7a7ae
Renee Nordholm <reneenordholm@gmail.com>
2019-05-13 23:02:10 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,15 +9,11 @@ class Song
   def initialize(name, artist, genre)
     @@count += 1
     @name = name
-    @artist = artist
+    @@artists << @artist
     @genre = genre
   end
 
   def self.count
     @@count
-  end
-  
-  def artists(artist)
-    @@artists[artist]
   end
 end
