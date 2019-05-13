2e0c944c1a907d83243fb4def6c06ffbd9a20ea8
Renee Nordholm <reneenordholm@gmail.com>
2019-05-13 22:52:27 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,11 +2,17 @@ class Song
   attr_accessor :name, :artist, :genre
   
   @@count = 0
+  @@genres = []
   
   def initialize
     @@count += 1
+    
   end
 
   def self.count
-    
+    @@count
+  end
+  
+  def 
+  
 end
